701c0dc8efd2fbdad41c8bc52a85c35a6bcaac6d
yuan qihang <qihang.yuan@thoughtworks.com>
2020-11-16 22:01:26 +0800

    complete test 1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,13 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.Year;
+import java.time.temporal.ChronoUnit;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +18,10 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    LocalDate laborDay = LocalDate.of(Integer.parseInt(Year.now().toString()), 5, 1);
+    if(date.isBefore(laborDay)){
+      return DAYS.between(date,laborDay);
+    }
+    return DAYS.between(date,laborDay.plusYears(1));
   }
 }
